b6607373e974a8388cb93f9b675568abe111aeb9
silvermistshadow <bmead72@gmail.com>
2019-02-11 09:34:39 -0800

    write all the code. all of it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
-  
+  $("#formOne").submit(function(event){
+    var yournameInput = $("input#yourname").val();
+
+    $(".yourname").text(yournameInput);
+
+    $("#letterbody").show();
+
+    event.preventDefault();
+
+  });
 
 });
